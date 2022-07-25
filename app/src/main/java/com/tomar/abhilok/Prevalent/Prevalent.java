69ebe0ad0d80641f5b27d246d2e3dcb55d5b3821
Alok1Tomar <76764786+Alok1Tomar@users.noreply.github.com>
2022-07-25 20:05:01 +0530

    Add files via upload

---
+++ app/src/main/java/com/tomar/abhilok/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.tomar.abhilok.Prevalent;
+
+import com.tomar.abhilok.Model.Users;
+
+public class Prevalent
+{
+    private static Users currentOnlineUser;
+
+    private static final String UserPhonekey ="UserPhone";
+    private static final String UserPasswordkey ="UserPassword";
+}
